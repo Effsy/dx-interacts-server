5ec67017c0a55b4ca5e08dd6c2f7af13d3531079
sebaraba <rabasebastians@gmail.com>
2019-02-25 20:34:54 +0000

    add needed functions

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ const app = express();
 
 // Local Provider
 web3js = new web3(new web3.providers.WebsocketProvider("http://127.0.0.1:8545"));
-var myAddress = '0x30abac1cec520be49ac535f81860368f78077785'; //CHANGE
-var toAddress = '0x5ffaa00c67230ef9424604e2e4abc58848d614cf';
-var privateKey = '0x5409626185b407dfc268568ef4de76df3dd412a5c78baf45354554ab326541b7'; //CHANGE
+var myAddress = '0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1'; //CHANGE
+var toAddress = '0xffcf8fdee72ac11b5c542428b35eef5769c409f0';
+var privateKey = '0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'; //CHANGE
 
-const dutchXContractAddress = "0xB3e96188da9B4d928093aB5640F47085cda4ec67"; // proxy contract address //CHANGE
+const dutchXContractAddress = "0xD86C8F0327494034F60e25074420BcCF560D5610"; // proxy contract address //CHANGE
 const dutchXContractABI = require('./dutchXabi.json'); // logic contract ABI
 const dutchXContract = new web3js.eth.Contract(dutchXContractABI, dutchXContractAddress);
 
-const owlContractAddress = '0x3C82f155a229365A04643d43F216A906a4A3537a'; // proxy contract address //CHANGE
+const owlContractAddress = '0xA57B8a5584442B467b4689F1144D269d096A3daF'; // proxy contract address //CHANGE
 const owlContractABI = require('./owlTokenabi.json');
 const owlContract = new web3js.eth.Contract(owlContractABI, owlContractAddress);
 
-const gnoContractAddress = '0xaD6279a07619b3B377240bf7Ef4bb5D45a7e6612'; //CHANGE
+const gnoContractAddress = '0x67B5656d60a809915323Bf2C40A8bEF15A152e3e'; //CHANGE
 const gnoContractABI = require('./gnoTokenabi.json');
 const gnoContract = new web3js.eth.Contract(gnoContractABI, gnoContractAddress);
 
-const wethContractAddress = '0x255E92C885293E719041B9e5A8633F603C09b79A'; //CHANGE
+const wethContractAddress = '0x9b1f7F645351AF3631a656421eD2e40f2802E6c0'; //CHANGE
 const wethContractABI = require('./etherTokenabi.json');
 const wethContract = new web3js.eth.Contract(wethContractABI, wethContractAddress);
 
@@ -54,6 +54,36 @@ app.get('/testSend', function (req, res) {
 });
 
 
+app.get('/testApprove', () => {
+    wethContract.methods.approve(myAddress, 1000000000).send({from: myAddress})
+        .on('transactionHash', (hash) => {
+            console.log("Approval successful! Transaction hash: " + hash);
+        })
+        .on('error', (error) => {
+            console.log("error");
+            console.log(error);
+        }).catch(err => {
+        console.log(err)
+    });
+});
+
+app.get('/postSellOrder', () => {
+    var auctionIndex = dutchXContract.methods.getAuctionIndex(owlContract, gnoContract);
+    console.log("AuctionINDEX" + auctionIndex);
+
+    dutchXContract.methods.postSellOrder(owlContractAddress, gnoContractAddress, 0, 1).send({from: myAddress})
+        .on('transactionHash', (hash) => {
+            console.log("Deposit successful! Transaction hash: " + hash);
+        })
+        .on('error', (error) => {
+            console.log("error");
+            console.log(error);
+        }).catch(err => {
+        console.log(err)
+
+    });
+});
+
 app.get('/testTransfer', () => {
     wethContract.methods.transfer(toAddress, 1000000000).send({from: myAddress})
         .on('transactionHash', (hash) => {
@@ -63,13 +93,14 @@ app.get('/testTransfer', () => {
             console.log("error");
             console.log(error);
         }).catch(err => {
-            console.log(err)
+        console.log(err)
     });
 });
 
 
+
 app.get('/testTransferFrom', () => {
-    wethContract.methods.transferFrom().send({from: myAddress, to: toAddress, value: 10000000000})
+    wethContract.methods.transferFrom(myAddress, toAddress, 1000000000000).send({from: myAddress})
         .on('transactionHash', (hash) => {
             console.log("Deposit successful! Transaction hash: " + hash);
         })
@@ -162,7 +193,7 @@ app.listen(3000, () => {
             console.log("error");
             console.log(err);
         } else {
-            console.log("WETH Deposit Succesfull - Event received");
+            console.log("WETH Approval Succesfull - Event received");
             console.log(res);
         }
 
