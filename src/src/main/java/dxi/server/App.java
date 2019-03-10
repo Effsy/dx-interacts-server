d96f93414389636326fa734027f63deb33c47eb4
Frederico A Lacs <fredlacs@gmail.com>
2019-03-10 15:08:22 +0000

    Add post sell order functionality to server

--- src/src/main/java/dxi/server/App.java
+++ src/src/main/java/dxi/server/App.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.ClientTransactionManager;
 import org.web3j.tx.gas.DefaultGasProvider;
@@ -27,29 +28,81 @@ public class App {
         return web3.ethAccounts().send().getAccounts();
     }
 
+    private static BigInteger toWei(Long l) {
+        var ethValue = BigInteger.valueOf(l).multiply(BigInteger.valueOf(10).pow(18));
+        return ethValue;
+    }
+
     public static void main(String[] args) throws Exception {
         
-        // var accounts = web3.ethAccounts().send().getAccounts();
         var accounts = getAccounts();
         var ctm1 = new ClientTransactionManager(web3, accounts.get(0));
         var ctm2 = new ClientTransactionManager(web3, accounts.get(1));
-
-
+        
+        
         String dutchExchangeAddress = "0x13274fe19c0178208bcbee397af8167a7be27f6f";
         String dxInteractsAddress = "0x2a504b5e7ec284aca5b6f49716611237239f0b97";
         String wethAddress = "0x345ca3e014aaf5dca488057592ee47305d9b3e10";
+        String gnoAddress = "0x8f0483125fcb9aaaefa9209d8e9d7b9c8b9fb90f";
+        
 
+        // to call functions with different accounts, choose a different ClientTransactionManager
         DutchExchange dx = new DutchExchange(dutchExchangeAddress, web3, ctm1, gasProvider);
         DxInteracts dxi = new DxInteracts(dxInteractsAddress, web3, ctm1, gasProvider);
-        EtherToken weth = new EtherToken(wethAddress, web3, ctm1, gasProvider);
+        TokenGNO gno = new TokenGNO(gnoAddress, web3, ctm1, gasProvider);
+        // EtherToken weth = new EtherToken(wethAddress, web3, ctm1, gasProvider);
+        
 
-        var balance = dx.balances(wethAddress, dxi.getContractAddress()).send();
-        System.out.println("weth balance in dx of acc[0]: " + balance.toString());
+        var startingETH = toWei(20L);
+        var startingGNO = toWei(50L);
+
+        var prevWethBalance = dx.balances(wethAddress, dxi.getContractAddress()).send();
+        dxi.depositEther(startingETH).send();
+        var postWethBalance = dx.balances(wethAddress, dxi.getContractAddress()).send();
         
-        BigInteger weiValue = BigInteger.valueOf((long) 1e18/1000000000);
-        var res = dxi.depositEther(weiValue).send();
+        // Transfering initial supply of GNO to dxi
+        gno.transfer(dxi.getContractAddress(), startingGNO).send();
         
-        balance = dx.balances(wethAddress, dxi.getContractAddress()).send();
-        System.out.println("weth balance in dx of acc[0]: " + balance.toString());
+        var prevGnoBalance = dx.balances(gnoAddress, dxi.getContractAddress()).send();
+        dxi.depositToken(gnoAddress, startingGNO).send();
+        var postGnoBalance = dx.balances(gnoAddress, dxi.getContractAddress()).send();
+        
+        
+        System.out.println("weth balance of dxi in dx: " + prevWethBalance);
+        System.out.println("weth balance of dxi in dx: " + postWethBalance);
+        System.out.println("gno balance of dxi in dx: " + prevGnoBalance);
+        System.out.println("gno balance of dxi in dx: " + postGnoBalance);
+        
+        var token1Funding = toWei(10L);
+        var token2Funding = BigInteger.valueOf(0L);
+        var initialClosingPriceNum = BigInteger.valueOf(2L);
+        var initialClosingPriceDen = BigInteger.valueOf(1L);
+
+        var startBlock = DefaultBlockParameter.valueOf("earliest");
+        var endBlock = DefaultBlockParameter.valueOf("latest");
+        
+        dx.newTokenPairEventFlowable(startBlock, endBlock).subscribe(e -> {
+            System.out.println("new token pair");
+            System.out.println("buy token: " + e.buyToken + ", sell token: " + e.sellToken);
+            System.out.println(e.sellToken.equals(wethAddress) + " " + e.buyToken.equals(gnoAddress));
+        });
+        
+        dx.newSellOrderEventFlowable(startBlock, endBlock).subscribe(e -> {
+            System.out.println("new sell order");
+            System.out.println("buy token: " + e.buyToken + ", sell token: " + e.sellToken + ", amount: " + e.amount);
+            System.out.println(e.sellToken.equals(wethAddress) + " " + e.buyToken.equals(gnoAddress));
+        });
+
+        dxi.addTokenPair(wethAddress, gnoAddress, token1Funding, token2Funding, initialClosingPriceNum, initialClosingPriceDen).send();
+
+        // System.out.println("here1");
+        // Thread.sleep(10000);
+        // System.out.println("here2");
+
+        // dxi.postSellOrder(wethAddress, gnoAddress, auctionIndex, sellOrderAmount).send();
+
+        var auctionIndex = dx.getAuctionIndex(wethAddress, gnoAddress).send();
+        var sellOrderAmount = BigInteger.valueOf(10000L);
+        dxi.postSellOrder(wethAddress, gnoAddress, auctionIndex, sellOrderAmount).send();
     }
 }
---
+++ src/src/main/java/dxi/server/TokenGNO.java
@@ -0,0 +1,302 @@
+package dxi.server;
+
+import io.reactivex.Flowable;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import org.web3j.abi.EventEncoder;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Event;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.generated.Uint8;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 4.1.1.
+ */
+public class TokenGNO extends Contract {
+    private static final String BINARY = "0x608060405234801561001057600080fd5b506040516020806106148339810180604052602081101561003057600080fd5b50516002819055336000908152602081905260409020556105be806100566000396000f3fe608060405234801561001057600080fd5b50600436106100b0576000357c010000000000000000000000000000000000000000000000000000000090048063313ce56711610083578063313ce567146101c257806370a08231146101e057806395d89b4114610206578063a9059cbb1461020e578063dd62ed3e1461023a576100b0565b806306fdde03146100b5578063095ea7b31461013257806318160ddd1461017257806323b872dd1461018c575b600080fd5b6100bd610268565b6040805160208082528351818301528351919283929083019185019080838360005b838110156100f75781810151838201526020016100df565b50505050905090810190601f1680156101245780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b61015e6004803603604081101561014857600080fd5b50600160a060020a03813516906020013561029f565b604080519115158252519081900360200190f35b61017a610306565b60408051918252519081900360200190f35b61015e600480360360608110156101a257600080fd5b50600160a060020a0381358116916020810135909116906040013561030c565b6101ca610439565b6040805160ff9092168252519081900360200190f35b61017a600480360360208110156101f657600080fd5b5035600160a060020a031661043e565b6100bd610459565b61015e6004803603604081101561022457600080fd5b50600160a060020a038135169060200135610490565b61017a6004803603604081101561025057600080fd5b50600160a060020a038135811691602001351661055b565b60408051808201909152600681527f476e6f7369730000000000000000000000000000000000000000000000000000602082015281565b336000818152600160209081526040808320600160a060020a038716808552908352818420869055815186815291519394909390927f8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925928290030190a35060015b92915050565b60025490565b600160a060020a038316600090815260208190526040812054610335908363ffffffff61058616565b15806103725750600160a060020a0384166000908152600160209081526040808320338452909152902054610370908363ffffffff61058616565b155b806103a35750600160a060020a0383166000908152602081905260409020546103a1908363ffffffff61058b16565b155b156103b057506000610432565b600160a060020a038085166000818152602081815260408083208054889003905560018252808320338452825280832080548890039055938716808352828252918490208054870190558351868152935191937fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef929081900390910190a35060015b9392505050565b601281565b600160a060020a031660009081526020819052604090205490565b60408051808201909152600381527f474e4f0000000000000000000000000000000000000000000000000000000000602082015281565b336000908152602081905260408120546104b0908363ffffffff61058616565b15806104e25750600160a060020a0383166000908152602081905260409020546104e0908363ffffffff61058b16565b155b156104ef57506000610300565b3360008181526020818152604080832080548790039055600160a060020a03871680845292819020805487019055805186815290519293927fddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef929181900390910190a350600192915050565b600160a060020a03918216600090815260016020908152604080832093909416825291909152205490565b111590565b810110159056fea165627a7a723058200a2db817bddecb3cc641ab42ca66225c76a6ed571007795e261be3487e9425e00029";
+
+    public static final String FUNC_NAME = "name";
+
+    public static final String FUNC_APPROVE = "approve";
+
+    public static final String FUNC_TOTALSUPPLY = "totalSupply";
+
+    public static final String FUNC_TRANSFERFROM = "transferFrom";
+
+    public static final String FUNC_DECIMALS = "decimals";
+
+    public static final String FUNC_BALANCEOF = "balanceOf";
+
+    public static final String FUNC_SYMBOL = "symbol";
+
+    public static final String FUNC_TRANSFER = "transfer";
+
+    public static final String FUNC_ALLOWANCE = "allowance";
+
+    public static final Event TRANSFER_EVENT = new Event("Transfer", 
+            Arrays.<TypeReference<?>>asList(new TypeReference<Address>(true) {}, new TypeReference<Address>(true) {}, new TypeReference<Uint256>() {}));
+    ;
+
+    public static final Event APPROVAL_EVENT = new Event("Approval", 
+            Arrays.<TypeReference<?>>asList(new TypeReference<Address>(true) {}, new TypeReference<Address>(true) {}, new TypeReference<Uint256>() {}));
+    ;
+
+    protected static final HashMap<String, String> _addresses;
+
+    static {
+        _addresses = new HashMap<String, String>();
+        _addresses.put("1552219502232", "0xaa588d3737b611bafd7bd713445b314bd453a5c8");
+    }
+
+    @Deprecated
+    protected TokenGNO(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected TokenGNO(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected TokenGNO(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected TokenGNO(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteCall<String> name() {
+        final Function function = new Function(FUNC_NAME, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteCall<TransactionReceipt> approve(String spender, BigInteger value) {
+        final Function function = new Function(
+                FUNC_APPROVE, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(spender), 
+                new org.web3j.abi.datatypes.generated.Uint256(value)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<BigInteger> totalSupply() {
+        final Function function = new Function(FUNC_TOTALSUPPLY, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public RemoteCall<TransactionReceipt> transferFrom(String from, String to, BigInteger value) {
+        final Function function = new Function(
+                FUNC_TRANSFERFROM, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(from), 
+                new org.web3j.abi.datatypes.Address(to), 
+                new org.web3j.abi.datatypes.generated.Uint256(value)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<BigInteger> decimals() {
+        final Function function = new Function(FUNC_DECIMALS, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint8>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public RemoteCall<BigInteger> balanceOf(String owner) {
+        final Function function = new Function(FUNC_BALANCEOF, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(owner)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public RemoteCall<String> symbol() {
+        final Function function = new Function(FUNC_SYMBOL, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public RemoteCall<TransactionReceipt> transfer(String to, BigInteger value) {
+        final Function function = new Function(
+                FUNC_TRANSFER, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(to), 
+                new org.web3j.abi.datatypes.generated.Uint256(value)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<BigInteger> allowance(String owner, String spender) {
+        final Function function = new Function(FUNC_ALLOWANCE, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Address(owner), 
+                new org.web3j.abi.datatypes.Address(spender)), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Uint256>() {}));
+        return executeRemoteCallSingleValueReturn(function, BigInteger.class);
+    }
+
+    public List<TransferEventResponse> getTransferEvents(TransactionReceipt transactionReceipt) {
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(TRANSFER_EVENT, transactionReceipt);
+        ArrayList<TransferEventResponse> responses = new ArrayList<TransferEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            TransferEventResponse typedResponse = new TransferEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse.from = (String) eventValues.getIndexedValues().get(0).getValue();
+            typedResponse.to = (String) eventValues.getIndexedValues().get(1).getValue();
+            typedResponse.value = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Flowable<TransferEventResponse> transferEventFlowable(EthFilter filter) {
+        return web3j.ethLogFlowable(filter).map(new io.reactivex.functions.Function<Log, TransferEventResponse>() {
+            @Override
+            public TransferEventResponse apply(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(TRANSFER_EVENT, log);
+                TransferEventResponse typedResponse = new TransferEventResponse();
+                typedResponse.log = log;
+                typedResponse.from = (String) eventValues.getIndexedValues().get(0).getValue();
+                typedResponse.to = (String) eventValues.getIndexedValues().get(1).getValue();
+                typedResponse.value = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public Flowable<TransferEventResponse> transferEventFlowable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(TRANSFER_EVENT));
+        return transferEventFlowable(filter);
+    }
+
+    public List<ApprovalEventResponse> getApprovalEvents(TransactionReceipt transactionReceipt) {
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(APPROVAL_EVENT, transactionReceipt);
+        ArrayList<ApprovalEventResponse> responses = new ArrayList<ApprovalEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            ApprovalEventResponse typedResponse = new ApprovalEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse.owner = (String) eventValues.getIndexedValues().get(0).getValue();
+            typedResponse.spender = (String) eventValues.getIndexedValues().get(1).getValue();
+            typedResponse.value = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Flowable<ApprovalEventResponse> approvalEventFlowable(EthFilter filter) {
+        return web3j.ethLogFlowable(filter).map(new io.reactivex.functions.Function<Log, ApprovalEventResponse>() {
+            @Override
+            public ApprovalEventResponse apply(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(APPROVAL_EVENT, log);
+                ApprovalEventResponse typedResponse = new ApprovalEventResponse();
+                typedResponse.log = log;
+                typedResponse.owner = (String) eventValues.getIndexedValues().get(0).getValue();
+                typedResponse.spender = (String) eventValues.getIndexedValues().get(1).getValue();
+                typedResponse.value = (BigInteger) eventValues.getNonIndexedValues().get(0).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public Flowable<ApprovalEventResponse> approvalEventFlowable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(APPROVAL_EVENT));
+        return approvalEventFlowable(filter);
+    }
+
+    @Deprecated
+    public static TokenGNO load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new TokenGNO(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static TokenGNO load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new TokenGNO(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static TokenGNO load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return new TokenGNO(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static TokenGNO load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new TokenGNO(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<TokenGNO> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider, BigInteger amount) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(amount)));
+        return deployRemoteCall(TokenGNO.class, web3j, credentials, contractGasProvider, BINARY, encodedConstructor);
+    }
+
+    public static RemoteCall<TokenGNO> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider, BigInteger amount) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(amount)));
+        return deployRemoteCall(TokenGNO.class, web3j, transactionManager, contractGasProvider, BINARY, encodedConstructor);
+    }
+
+    @Deprecated
+    public static RemoteCall<TokenGNO> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit, BigInteger amount) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(amount)));
+        return deployRemoteCall(TokenGNO.class, web3j, credentials, gasPrice, gasLimit, BINARY, encodedConstructor);
+    }
+
+    @Deprecated
+    public static RemoteCall<TokenGNO> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit, BigInteger amount) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint256(amount)));
+        return deployRemoteCall(TokenGNO.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, encodedConstructor);
+    }
+
+    protected String getStaticDeployedAddress(String networkId) {
+        return _addresses.get(networkId);
+    }
+
+    public static String getPreviouslyDeployedAddress(String networkId) {
+        return _addresses.get(networkId);
+    }
+
+    public static class TransferEventResponse {
+        public Log log;
+
+        public String from;
+
+        public String to;
+
+        public BigInteger value;
+    }
+
+    public static class ApprovalEventResponse {
+        public Log log;
+
+        public String owner;
+
+        public String spender;
+
+        public BigInteger value;
+    }
+}
