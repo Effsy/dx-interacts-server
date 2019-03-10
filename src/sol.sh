d43b90514ddfd8a6b26e6568ad3771cf8276dbf3
Frederico A Lacs <fredlacs@gmail.com>
2019-03-10 18:44:30 +0000

    Add all necessary contract wrappers from dx-interacts repo

--- src/sol.sh
+++ src/sol.sh
@@ -1,6 +1,6 @@
-dxiTruffleOutputPath="../../dx-contracts/build/contracts/DxInteracts.json"
-dxTruffleOutputPath="../../dx-contracts/build/contracts/DutchExchange.json"
-gnoTruffleOutputPath="../../dx-contracts/build/contracts/TokenGNO.json"
+dxiTruffleOutputPath="../../dx-interacts/build/contracts/DxInteracts.json"
+dxTruffleOutputPath="../../dx-interacts/build/contracts/DutchExchange.json"
+gnoTruffleOutputPath="../../dx-interacts/build/contracts/TokenGNO.json"
 web3j truffle generate $dxiTruffleOutputPath -o ./src/main/java -p dxi.server
 web3j truffle generate $dxTruffleOutputPath -o ./src/main/java -p dxi.server
 web3j truffle generate $gnoTruffleOutputPath -o ./src/main/java -p dxi.server
