89d7b646d1bb22e4cf5d72c2912e7bd53a4f99a2
Frederico A Lacs <fredlacs@gmail.com>
2019-03-10 17:57:54 +0000

    Compile all necessary wrappers

--- src/sol.sh
+++ src/sol.sh
@@ -1,2 +1,6 @@
-truffleOutputPath="../../dx-contracts/build/contracts/DxInteracts.json"
-web3j truffle generate $truffleOutputPath -o ./src/main/java -p dxi.server
+dxiTruffleOutputPath="../../dx-contracts/build/contracts/DxInteracts.json"
+dxTruffleOutputPath="../../dx-contracts/build/contracts/DutchExchange.json"
+gnoTruffleOutputPath="../../dx-contracts/build/contracts/TokenGNO.json"
+web3j truffle generate $dxiTruffleOutputPath -o ./src/main/java -p dxi.server
+web3j truffle generate $dxTruffleOutputPath -o ./src/main/java -p dxi.server
+web3j truffle generate $gnoTruffleOutputPath -o ./src/main/java -p dxi.server
