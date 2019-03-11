05f275246804ccc1ee83724a9bcde3993713af45
Frederico A Lacs <fredlacs@gmail.com>
2019-03-11 00:13:32 +0000

    Separate dx-interacts dir from specific file name

--- src/sol.sh
+++ src/sol.sh
@@ -1,6 +1,9 @@
-dxiTruffleOutputPath="../../dx-interacts/build/contracts/DxInteracts.json"
-dxTruffleOutputPath="../../dx-interacts/build/contracts/DutchExchange.json"
-gnoTruffleOutputPath="../../dx-interacts/build/contracts/TokenGNO.json"
+dxiDir="../../dx-interacts/build/contracts/"
+
+dxiTruffleOutputPath=$dxiDir"DxInteracts.json"
+dxTruffleOutputPath=$dxiDir"DutchExchange.json"
+gnoTruffleOutputPath=$dxiDir"TokenGNO.json"
+
 web3j truffle generate $dxiTruffleOutputPath -o ./src/main/java -p dxi.server
 web3j truffle generate $dxTruffleOutputPath -o ./src/main/java -p dxi.server
 web3j truffle generate $gnoTruffleOutputPath -o ./src/main/java -p dxi.server
